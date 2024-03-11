971ddbb00c071678d6c5d1b7a8caf250674f54c1
Johnn333 <47667973+Johnn333@users.noreply.github.com>
2024-03-11 03:29:45 +0000

    Some work done on displaying reduced times based on intel lab level

--- script.js
+++ script.js
@@ -1,3 +1,75 @@
+// Upgrade times D/H/M
+const upgradeTimes = {
+    "U1": [0,19,22],
+    "U2": [2,10,0],
+    "U3": [4,20,0],
+    "X1": [6,19,0]
+}
+
+// Flag upgrade times D/H/M
+const upgradeTimesFlag = {
+    "U1": [0,22,26],
+    "U2": [2,19,0],
+    "U3": [5,15,0],
+    "X1": [10,4,0]
+}
+
+// Copy above values, to keep them around
+const tempUpgradeTimes = JSON.parse(JSON.stringify(upgradeTimes));
+const tempUpgradeTimesFlag = JSON.parse(JSON.stringify(upgradeTimesFlag));
+
+function updateTable(){
+    document.getElementById("U1TimeFlag").innerText = toTime("U1",1);
+    document.getElementById("U2TimeFlag").innerText = toTime("U2",1); 
+    document.getElementById("U3TimeFlag").innerText = toTime("U3",1); 
+    document.getElementById("X1TimeFlag").innerText = toTime("X1",1);
+
+    document.getElementById("U1Time").innerText = "| "+toTime("U1",0);
+    document.getElementById("U2Time").innerText = "| "+toTime("U2",0); 
+    document.getElementById("U3Time").innerText = "| "+toTime("U3",0); 
+    document.getElementById("X1Time").innerText = "| "+toTime("X1",0);
+}
+updateTable();
+
+function reduceTime(reduction){
+    for(upgrade in upgradeTimes){
+        let minutes = upgradeTimes[upgrade][0]*24*60+upgradeTimes[upgrade][1]*60+upgradeTimes[upgrade][2];
+
+        minutes = minutes*(1-(reduction/100));
+
+        let days = Math.floor(minutes / 1440);
+        let remainingTime = parseInt(minutes - Math.floor((days *1440)));
+        let hours = Math.floor((remainingTime / 60));
+        let remainingMin = Math.floor(remainingTime - (hours*60));
+
+        tempUpgradeTimes[upgrade][0] = days; // New Days
+        tempUpgradeTimes[upgrade][1] = hours;
+        tempUpgradeTimes[upgrade][2] = remainingMin;
+    }
+
+    for(upgrade in tempUpgradeTimesFlag){
+        let minutes = upgradeTimesFlag[upgrade][0]*24*60+upgradeTimesFlag[upgrade][1]*60+upgradeTimesFlag[upgrade][2];
+
+        minutes = minutes*(1-(reduction/100));
+
+        let days = Math.floor(minutes / 1440);
+        let remainingTime = parseInt(minutes - Math.floor((days *1440)));
+        let hours = Math.floor((remainingTime / 60));
+        let remainingMin = Math.floor(remainingTime - (hours*60));
+
+        tempUpgradeTimesFlag[upgrade][0] = days; // New Days
+        tempUpgradeTimesFlag[upgrade][1] = hours;
+        tempUpgradeTimesFlag[upgrade][2] = remainingMin;
+    }
+
+    updateTable();
+}
+
+function toTime(upgradeLevel, flagFlag){
+    if(flagFlag) return tempUpgradeTimesFlag[upgradeLevel][0]+"d "+tempUpgradeTimesFlag[upgradeLevel][1]+"h "+tempUpgradeTimesFlag[upgradeLevel][2]+"m";
+    return upgradeTimes[upgradeLevel][0]+"d "+tempUpgradeTimes[upgradeLevel][1]+"h "+tempUpgradeTimes[upgradeLevel][2]+"m";
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     const upgradesContainers = document.getElementsByClassName("upgrades");
 
@@ -59,6 +131,12 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         });
     }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("intelLevel").addEventListener("change", function(input){
+        reduceTime(parseInt(input.target.value));
+    })
 });
 
 // Function called to update the stats based on given upgrade levels.
